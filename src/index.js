c9cd5d3ac1ad68e5aa7120801709d1f2f9aa29f0
Lokesh Baviskar <101720181+22karlokesh@users.noreply.github.com>
2025-09-07 23:10:35 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,73 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./components/App";
+
+
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+
+// my code
+
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import { createStore } from "redux";
+import { Provider, useDispatch, useSelector } from "react-redux";
 
+// ---------------- Redux ----------------
+const initialState = { name: "", email: "" };
 
+const setName = (name) => ({ type: "SET_NAME", payload: name });
+const setEmail = (email) => ({ type: "SET_EMAIL", payload: email });
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function userReducer(state = initialState, action) {
+  switch (action.type) {
+    case "SET_NAME":
+      return { ...state, name: action.payload };
+    case "SET_EMAIL":
+      return { ...state, email: action.payload };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(userReducer);
+
+// ---------------- Component ----------------
+const UserInfo = () => {
+  const dispatch = useDispatch();
+  const { name, email } = useSelector((state) => state);
+
+  return (
+    <div>
+      <h1>User Information</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="Enter name"
+          value={name}
+          onChange={(e) => dispatch(setName(e.target.value))}
+        />
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => dispatch(setEmail(e.target.value))}
+        />
+      </form>
+
+      <div className="output">
+        <p>Name - {name}</p>
+        <p>Email - {email}</p>
+      </div>
+    </div>
+  );
+};
+
+// ---------------- Render ----------------
+ReactDOM.render(
+  <Provider store={store}>
+    <UserInfo />
+  </Provider>,
+  document.getElementById("root")
+);
